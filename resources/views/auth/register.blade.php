d487e30816e2c2bd6ad9dd6b73565d23ae1280aa
TomsBaders <toms.baders@gmail.com>
2023-05-20 10:16:31 +0300

    22nd Commit!

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,46 +11,43 @@
 </head>
 <body>
 <div class="container">
-                <h2>Registration</h2>
-                    <form method="POST" autocomplete="off" action="{{ route('register') }}">
-                                @csrf
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" min="4" max="20" required placeholder="Username">
+    <h2>Registration</h2>
+    <form method="POST" autocomplete="off" action="{{ route('register') }}">
+        @csrf
+        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" min="4" max="20" required placeholder="Username">
+        @error('name')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" min="6" max="30" required placeholder="E-Mail">
+        @error('email')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
 
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" min="6" max="30" required placeholder="E-Mail">
+        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" min="8" max="20" placeholder="Password">
+        @error('password')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" min="8" max="20" placeholder="Password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" min="8" max="20" placeholder="Repeat Password">
-                                <div class="text-center">
-                                <button type="submit" class="btn btn-success">
-                                    {{ __('Register') }}
-                                </button>
-                                </div>
-                                <br><br>
-                                @if (Route::has('login'))
-                                <div class="text-center">
-                                <h2>Are you registered?</h2>
-                                <a href="{{ route('login') }}" class="btn btn-primary text-center" style="align-self: center">Login</a>
-                                </div>
-                                @endif
-                    </form>
-         </div>
+        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" min="8" max="20" placeholder="Repeat Password">
+        <div class="text-center">
+            <button type="submit" class="btn btn-success">
+                {{ __('Register') }}
+            </button>
+        </div>
+        <br><br>
+        @if (Route::has('login'))
+            <div class="text-center">
+                <h2>Are you registered?</h2>
+                <a href="{{ route('login') }}" class="btn btn-primary text-center" style="align-self: center">Login</a>
+            </div>
+        @endif
+    </form>
+</div>
 </body>
