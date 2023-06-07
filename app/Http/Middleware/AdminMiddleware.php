3927a1adf7cb0eb1f68b4332c4c26045801a38f6
TomsBaders <toms.baders@gmail.com>
2023-06-07 09:31:23 +0300

    31st Commit.

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 class AdminMiddleware
 {
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check() && Auth::user()->is_admin) {
+            return $next($request);
+        }
 
+        return redirect('/');
+    }
 }
