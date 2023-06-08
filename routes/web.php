c695f32f3490eee48157728306b5855da623db31
TomsBaders <toms.baders@gmail.com>
2023-06-08 11:38:38 +0300

    32nd Commit.

--- routes/web.php
+++ routes/web.php
@@ -75,24 +75,24 @@ Route::middleware('auth')->group(function () {
 // Admin User Confirmation
     Route::middleware('admin')->group(function () {
 // Admin Page Routes
-        Route::get('/admin/purchases', [App\Http\Controllers\AdminController::class, 'purchasedashboard'])->name('adminpurchases');
-        Route::get('/admin/products', [App\Http\Controllers\AdminController::class, 'productdashboard'])->name('adminproducts');
-        Route::get('/admin/users', [App\Http\Controllers\AdminController::class, 'userdashboard'])->name('adminusers');
+    Route::get('/admin/purchases', [App\Http\Controllers\AdminController::class, 'purchasedashboard'])->name('adminpurchases');
+    Route::get('/admin/products', [App\Http\Controllers\AdminController::class, 'productdashboard'])->name('adminproducts');
+    Route::get('/admin/users', [App\Http\Controllers\AdminController::class, 'userdashboard'])->name('adminusers');
 // Admin Edit, Add and Delete Routes
-        Route::put('/admin/user/update/{id}', [App\Http\Controllers\AdminModifyController::class, 'adminmodifyuser'])->name('adminuserupdate');
-        Route::put('/admin/product/update/{id}', [App\Http\Controllers\AdminModifyController::class, 'adminproductupdate'])->name('adminproductupdate');
-        Route::delete('/admin/product/remove/{id}', [App\Http\Controllers\AdminModifyController::class, 'adminremoveproduct'])->name('adminremoveproduct');
-        Route::delete('/admin/purchase/remove/{id}', [App\Http\Controllers\AdminModifyController::class, 'adminremovepurchase'])->name('adminremovepurchase');
-        Route::delete('/admin/user/remove/{id}', [App\Http\Controllers\AdminModifyController::class, 'adminremoveuser'])->name('adminremoveuser');
+    Route::put('/admin/user/update/{id}', [App\Http\Controllers\AdminModifyController::class, 'adminmodifyuser'])->name('adminuserupdate');
+    Route::put('/admin/product/update/{id}', [App\Http\Controllers\AdminModifyController::class, 'adminproductupdate'])->name('adminproductupdate');
+    Route::delete('/admin/product/remove/{id}', [App\Http\Controllers\AdminModifyController::class, 'adminremoveproduct'])->name('adminremoveproduct');
+    Route::delete('/admin/purchase/remove/{id}', [App\Http\Controllers\AdminModifyController::class, 'adminremovepurchase'])->name('adminremovepurchase');
+    Route::delete('/admin/user/remove/{id}', [App\Http\Controllers\AdminModifyController::class, 'adminremoveuser'])->name('adminremoveuser');
 // Admin Search and Date Routes
-        Route::get('/AdminPurchaseSearchI', [App\Http\Controllers\AdminSearchController::class, 'adminpurchasesearchid'])->name('adminpurchasesearchid');
-        Route::get('/AdminPurchaseSearchU', [App\Http\Controllers\AdminSearchController::class, 'adminpurchasesearchuserid'])->name('adminpurchasesearchuserid');
-        Route::get('/AdminProductsSearchN', [App\Http\Controllers\AdminSearchController::class, 'adminproductssearchname'])->name('adminproductssearchname');
-        Route::get('/AdminProductsSearchU', [App\Http\Controllers\AdminSearchController::class, 'adminproductssearchuserid'])->name('adminproductssearchuserid');
-        Route::get('/AdminUserSearchI', [App\Http\Controllers\AdminSearchController::class, 'adminusersearchID'])->name('adminusersearchid');
-        Route::get('/AdminUserSearchN', [App\Http\Controllers\AdminSearchController::class, 'adminusersearchname'])->name('adminusersearchname');
-        Route::get('/AdminPurchaseDate', [App\Http\Controllers\AdminDateController::class, 'adminpurchasesdate'])->name('adminpurchasesdate');
-        Route::get('/AdminProductsDate', [App\Http\Controllers\AdminDateController::class, 'adminproductsdate'])->name('adminproductsdate');
-        Route::get('/AdminUserDate', [App\Http\Controllers\AdminDateController::class, 'adminuserdate'])->name('adminuserdate');
+    Route::get('/AdminPurchaseSearchI', [App\Http\Controllers\AdminSearchController::class, 'adminpurchasesearchid'])->name('adminpurchasesearchid');
+    Route::get('/AdminPurchaseSearchU', [App\Http\Controllers\AdminSearchController::class, 'adminpurchasesearchuserid'])->name('adminpurchasesearchuserid');
+    Route::get('/AdminProductsSearchN', [App\Http\Controllers\AdminSearchController::class, 'adminproductssearchname'])->name('adminproductssearchname');
+    Route::get('/AdminProductsSearchU', [App\Http\Controllers\AdminSearchController::class, 'adminproductssearchuserid'])->name('adminproductssearchuserid');
+    Route::get('/AdminUserSearchI', [App\Http\Controllers\AdminSearchController::class, 'adminusersearchID'])->name('adminusersearchid');
+    Route::get('/AdminUserSearchN', [App\Http\Controllers\AdminSearchController::class, 'adminusersearchname'])->name('adminusersearchname');
+    Route::get('/AdminPurchaseDate', [App\Http\Controllers\AdminDateController::class, 'adminpurchasesdate'])->name('adminpurchasesdate');
+    Route::get('/AdminProductsDate', [App\Http\Controllers\AdminDateController::class, 'adminproductsdate'])->name('adminproductsdate');
+    Route::get('/AdminUserDate', [App\Http\Controllers\AdminDateController::class, 'adminuserdate'])->name('adminuserdate');
     });
 });
